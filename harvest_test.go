df590ea29466212bdca2b34573bb387caa2a0580
Tom Maiaroto <tom@shift8creative.com>
2014-07-27 21:47:32 -0500

    placeholder test

---
+++ harvest_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// TODO: Tests.
+
+func TestFacebookPublicMessagesByKeyword(t *testing.T) {
+	assert.Equal(t, "foo", "foo", "should be foo")
+
+}
