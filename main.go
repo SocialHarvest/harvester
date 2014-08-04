afb0a59d1245d9b77a03967fb9c51bbfc2ccd919
Tom Maiaroto <tom@shift8creative.com>
2014-08-04 01:43:36 -0500

    removed debugging call. facebook harvester temporarily not harvesting. twitter ok, but not complete.

--- main.go
+++ main.go
@@ -196,7 +196,7 @@ func main() {
 	setInitialSchedule()
 
 	// Search Facebook public posts using keywords in Social Harvest config
-	go FacebookPublicMessagesByKeyword()
+	//go FacebookPublicMessagesByKeyword()
 	// Search Facebook public feeds using account ids in Social Harvest config
 	//FacebookMessagesByAccount()
 	//
