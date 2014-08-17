ed5238e09d093d302d1c73ebaaca82e06ce266f3
Tom Maiaroto <tom@shift8creative.com>
2014-08-16 20:00:13 -0500

    removing test calls

--- main.go
+++ main.go
@@ -511,11 +511,11 @@ func main() {
 
 	// Immedate calls to use for testing during development
 	// Search Facebook public posts using keywords in Social Harvest config
-	go FacebookPublicMessagesByKeyword()
+	//go FacebookPublicMessagesByKeyword()
 	// Search Facebook public feeds using account ids in Social Harvest config
-	go FacebookMessagesByAccount()
+	//go FacebookMessagesByAccount()
 	// Search Twitter using keywords in Social Harvest config
-	go TwitterPublicMessagesByKeyword()
+	//go TwitterPublicMessagesByKeyword()
 
 	//harvester.YoutubeVideoSearch("obama")
 	///
