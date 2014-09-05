a4b7e057cdd1df1d46ac92bbda1d86e0ba6d49f0
Tom Maiaroto <tom@shift8creative.com>
2014-09-04 21:51:06 -0500

    increment patch version to match last commit

--- main.go
+++ main.go
@@ -570,7 +570,7 @@ func main() {
 	color.Cyan(" ___) | (_) | (__| | (_| | | |  _  | (_| | |   \\ V /  __/\\__ \\ |_ ")
 	color.Cyan("|____/ \\___/ \\___|_|\\__,_|_| |_| |_|\\__,_|_|    \\_/ \\___||___/\\__|")
 	//	color.Cyan("                                                                  ")
-	color.Yellow("_____________________________________________version 0.7.0-preview")
+	color.Yellow("_____________________________________________version 0.7.1-preview")
 	color.Cyan("   ")
 
 	// Optionally allow a config JSON file to be passed via command line
