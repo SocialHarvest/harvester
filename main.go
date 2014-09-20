ed8b9945fe017ec3adc0e4568aecd7c1268530de
Tom Maiaroto <tom@shift8creative.com>
2014-09-20 10:22:39 -0500

    disabling profiling

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 	"github.com/fatih/color"
 	"log"
 	"net/http"
-	_ "net/http/pprof"
+	// _ "net/http/pprof"
 	"os"
 	"reflect"
 	"runtime"
@@ -621,11 +621,11 @@ func main() {
 	}
 
 	// Debug - do not compile with this
-	runtime.SetBlockProfileRate(1)
-	// Start a profile server so information can be viewed using a web browser
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	// runtime.SetBlockProfileRate(1)
+	// // Start a profile server so information can be viewed using a web browser
+	// go func() {
+	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
+	// }()
 
 	// Banner (would appear twice if it came before bugsnag for some reason)
 	color.Cyan(" ____             _       _   _   _                           _  ")
